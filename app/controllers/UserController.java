320756d9ef397e253448b85c6a4f3b349f67cd37
DjewelMcDee <joelledepagne@hotmail.com>
2016-11-17 15:57:29 +0100

    ajout  partiel methode login

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -4,12 +4,15 @@ import models.Task;
 import play.data.validation.Required;
 import models.User;
 import play.mvc.Controller;
+import play.mvc.With;
 
 import java.util.List;
 
 /**
  * Created by Formation on 17/11/2016.
  */
+
+@With(Secure.class)
 public class UserController extends Controller{
 
     public static void create(){ render(); }
--- app/controllers/secure/security.java
+++
@@ -1,7 +0,0 @@
-package controllers.secure;
-
-/**
- * Created by formation on 17/11/16.
- */
-public class security {
-}
