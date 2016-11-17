cc550086bb7ae24b1a8607b07ab0f6cbfbedfff5
jfrm <ephrem.at.work@outlook.com>
2016-11-17 14:50:24 +0100

    user controller

--- app/models/User.java
+++ app/models/User.java
@@ -1,10 +1,19 @@
 package models;
 
-public class User {
+import play.db.jpa.Model;
+
+public class User extends Model {
 
 	String name;
 	String password;
 	String avatar;
+
+    public User (){}
+
+    public User (String name){
+        super();
+        this.name = name;
+    }
 	
 	public User(String name, String password, String avatar) {
 		super();
