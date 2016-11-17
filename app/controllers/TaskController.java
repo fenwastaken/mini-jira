81b44cbc8d94677f130201b3f330d7c8fb297221
fenwastaken <fenwastaken@fenwastaken-pc>
2016-11-17 16:25:26 +0100

    objects by JulienTM

--- app/controllers/TaskController.java
+++ app/controllers/TaskController.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class TaskController extends Controller{
     public static void create(){ render(); }
 
+    //add this back in the params once projects are manageable: @Required int idProject
     public static void save(@Required String name, @Required String content, @Required Boolean urgent){
         if (validation.hasErrors()) {
             params.flash();
@@ -22,6 +23,7 @@ public class TaskController extends Controller{
         task.name = name;
         task.content = content;
         task.urgent = urgent;
+        task.idProject = 1;
         task.save();
         //render(task);
         displayall();
